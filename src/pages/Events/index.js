1ed158936a24a0ce8145bcb1ab4d53e31e5fe088
A S M Taseen <taseen951753@gmail.com>
2022-02-16 19:48:17 -0500

    final changes

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
-import { Box, Text, Button, Input, Tag, Menu, MenuButton, MenuList, MenuItem, Center, Container } from '@chakra-ui/react'
+import { Box, Text, Button, Input, Tag, Menu, MenuButton, MenuList, MenuItem, Center } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
 
 import styles from "./index.module.css";
@@ -117,6 +117,7 @@ const Events = () => {
                             borderWidth="2px"
                             border="solid"
                             borderColor={"rgb(252, 250, 243)"}>
+                                
                             <Text display='inline-block' fontSize='lg' color={"white"}>{event?.name}</Text>
                             <Tag size={'sm'} key={'1'} marginLeft={'5px'} color={'rgb(29, 76, 130)'} fontSize="10px" variant='solid' backgroundColor={'rgb(250, 227, 19)'}>{event?.event_type.toUpperCase()}</Tag>
                             <Tag size={'sm'} key={'2'} marginLeft={'5px'} variant='solid' backgroundColor={'#4abd9a'}>
